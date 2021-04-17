b0babeef1d956d3afd175e4f158f59f24b7770e2
lmichitakal <m391gub@gmail.com>
2021-04-17 18:23:47 +0900

    a

---
+++ 20200417/a.py
@@ -0,0 +1,12 @@
+import sys
+import math
+from collections import *
+from itertools import *
+#n = int(input())
+x, y, z = map(int, input().split())
+#L = list(map(int, input().split()))
+#l = [int(input()) for i in range(n)]
+#ans = 0
+#print(ans)
+s = y/x
+print(max(0,math.ceil(s * z)-1))
